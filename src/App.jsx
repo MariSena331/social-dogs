385e17c36cecffc945c2c5e47a1a317a0cc1f3c9
Mariana Sena <35273542+MariSena331@users.noreply.github.com>
2025-01-23 18:19:27 -0300

    chore: inserting header and footer at main file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
 
   return (
     <div>
-      App React
+      <BrowserRouter>
+        <Header />
+        <Footer />
+      </BrowserRouter>
     </div>
   )
 }
 
-export default App
+export default App;
