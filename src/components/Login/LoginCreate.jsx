e66615bf110cbf7796da38e4d83e69570026fc98
Mariana Sena <35273542+MariSena331@users.noreply.github.com>
2025-01-31 17:22:17 -0300

    chore: adds login page

---
+++ src/components/Login/LoginCreate.jsx
@@ -0,0 +1,12 @@
+import PropTypes from "prop-types";
+import React, { Component } from "react";
+
+export class LoginCreate extends Component {
+  static propTypes = {};
+
+  render() {
+    return <div>Login Criar</div>;
+  }
+}
+
+export default LoginCreate;
